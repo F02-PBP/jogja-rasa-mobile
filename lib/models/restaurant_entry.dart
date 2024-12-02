a5e691aaa12690dcfe3e3ea1064751443b3e2316
gracekarinn <gracekarina12@gmail.com>
2024-12-02 23:35:36 +0700

    feat: add Restaurant model with JSON serialization and location determination

---
+++ lib/models/restaurant_entry.dart
@@ -0,0 +1,53 @@
+class Restaurant {
+  final String id;
+  final String name;
+  final double longitude;
+  final double latitude;
+  final String description;
+  bool isBookmarked;
+  double rating;
+  String location;
+
+  Restaurant({
+    required this.id,
+    required this.name,
+    required this.longitude,
+    required this.latitude,
+    required this.description,
+    this.isBookmarked = false,
+    this.rating = 0.0,
+    required this.location,
+  });
+
+  factory Restaurant.fromJson(Map<String, dynamic> json) {
+    return Restaurant(
+      id: json['id'],
+      name: json['name'],
+      longitude: json['longitude'].toDouble(),
+      latitude: json['latitude'].toDouble(),
+      description: json['description'],
+      location: json['location'] ??
+          getLocationFromCoordinates(
+              json['longitude'].toDouble(), json['latitude'].toDouble()),
+    );
+  }
+
+  static String getLocationFromCoordinates(double longitude, double latitude) {
+    if (longitude >= 110.70) {
+      return "Solo";
+    } else if (110.38 <= longitude && longitude <= 110.41 ||
+        longitude >= 110.41) {
+      return "Yogyakarta Timur";
+    } else if (110.35 <= longitude && longitude <= 110.38) {
+      return "Yogyakarta Pusat";
+    } else if (110.32 <= longitude && longitude <= 110.35 ||
+        longitude <= 110.32) {
+      return "Yogyakarta Barat";
+    } else if (-7.770 >= latitude && latitude >= -7.750) {
+      return "Yogyakarta Utara";
+    } else if (-7.820 <= latitude && latitude <= -7.810) {
+      return "Yogyakarta Selatan";
+    }
+    return "Yogyakarta";
+  }
+}
